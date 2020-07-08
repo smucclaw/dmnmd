905c2ab9920b1fbb6d1cef9c6ba08c318072943c
Inari Listenmaa <inari.listenmaa@gmail.com>
2020-07-08 15:01:43 +0200

    Update demo

--- languages/gf/demo.sh
+++ languages/gf/demo.sh
@@ -1,21 +1,13 @@
-#/usr/bin/bash
+/usr/bin/bash
 
-winter='Row 1 (Single (TimeSeason "Season" (FNullary (VS "Winter")))) (Single (Attribute "Dish" (FNullary (VS "Kidney bean stew")))) NoComment'
-spring='Row 2 (Single (TimeSeason "Season" (FNullary (VS "Spring")))) (Single (Attribute "Dish" (FNullary (VS "Smoked tofu salad")))) NoComment'
-summer='Row 3 (Single (TimeSeason "Season" (FNullary (VS "Summer")))) (Single (Attribute "Dish" (FNullary (VS  "Roasted potatoes and a nice steak")))) NoComment'
-autumn='Row 4 (Many (BaseFCell (TimeSeason "Season" (FNullary (VS "Autumn"))) (AmountCount "Guest" "count" (FInRangeInt 5 8)))) (Single (Attribute "Dish" (FNullary (VS "Instant noodles")))) NoComment'
-other='Row 5 (Many (BaseFCell (TimeSeason "Season" FAnything) (AmountCount "Guest" "count" FAnything))) (Single (Attribute "Dish" (FNullary (VS "Pea soup")))) (CommentString "I give up")'
+winter='Row 1 (Single (TimeSeason (ncn season_2_N) (FNullary (VS "Winter")))) (Single (Attribute (ncn dish_2_N) (FNullary (VS "Kidney bean stew")))) NoComment'
+spring='Row 2 (Single (TimeSeason (ncn season_2_N) (FNullary (VS "Spring")))) (Single (Attribute (ncn dish_2_N) (FNullary (VS "Smoked tofu salad")))) NoComment'
+summer='Row 3 (Single (TimeSeason (ncn season_2_N) (FNullary (VS "Summer")))) (Single (Attribute (ncn dish_2_N) (FNullary (VS  "Roasted potatoes and a nice steak")))) NoComment'
+autumn='Row 4 (Many (BaseFCell (TimeSeason (ncn season_2_N) (FNullary (VS "Autumn"))) (AmountCount (ncn guest_1_N) (ncn count_1_N)  (FInRangeInt 5 8)))) (Single (Attribute (ncn dish_2_N) (FNullary (VS "Instant noodles")))) NoComment'
+other='Row 5 (Many (BaseFCell (TimeSeason (ncn season_2_N) FAnything) (AmountCount (ncn guest_1_N) (ncn count_1_N)  FAnything))) (Single (Attribute (ncn dish_2_N) (FNullary (VS "Pea soup")))) (CommentString "I give up")'
 
 
 table="l -bind -treebank Table (ConsDTRow ($winter) (ConsDTRow ($spring) (ConsDTRow ($summer) (BaseDTRow ($autumn) ($other)))))"
 
-echo $table | gf --run DMNEng*.gf | sed -E 's/ \\ /\\/g ; s/: /\\---\\/g ; s/DMN/\\DMN/g' | tr '\' '\n'
+echo $table | gf --run DMN.pgf | sed -E 's/ \\ /\\/g ; s/: /\\---\\/g ; s/DMN/\\DMN/g' | tr '\' '\n'
 
-
-expiration='Row 1 (Single (Event "Event" (Disj (ConsFEELexp (FNullary (VS "Equity Financing")) (BaseFEELexp (FNullary (VS "Liquidity")) (FNullary (VS "Dissolution"))))))) (Single (Attribute "Contract" (FNullary (VS "Expired")))) NoComment'
-
-obligations='Row 2 (Single (Event "Event" (FNullary (VS "Expiration")))) (Single (Attribute "Companys obligations" (FNullary (VS "A, B and C")))) NoComment'
-
-consequence="l -bind -treebank Consequence (BaseDTRow ($obligations) ($expiration))"
-
-echo $consequence | gf --run DMNEng*.gf | sed -E 's/ \\ /\\/g ; s/: /\\---\\/g ; s/DMN/\\DMN/g' | tr '\' '\n'
