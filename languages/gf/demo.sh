ce7a27a9200da007d88f5ad6f86ee8ce7a4e9c34
Inari Listenmaa <inari.listenmaa@gmail.com>
2020-07-27 15:03:04 +0200

    Make demo.sh more helpful, remove unused files

--- languages/gf/demo.sh
+++ languages/gf/demo.sh
@@ -1,5 +1,20 @@
 #/usr/bin/bash
 
+# Build GF grammar, if needed
+if [ -n "$(ls -la | grep pgf)" ]
+then
+    echo "GF grammar found, now linearizing an example tree"
+else
+    echo "Building the GF grammar..."
+
+    echo "(This may take some time)"
+    gf -make --optimize-pgf DMNEng*.gf
+
+    echo "GF grammar built, now linearizing an example tree"
+fi
+
+# Linearize a tree
+
 winter='Row 1 (Single (TimeSeason (ncn season_2_N) (FNullary (VS "Winter")))) (Single (Attribute (ncn dish_2_N) (FNullary (VS "Kidney bean stew")))) NoComment'
 spring='Row 2 (Single (TimeSeason (ncn season_2_N) (FNullary (VS "Spring")))) (Single (Attribute (ncn dish_2_N) (FNullary (VS "Smoked tofu salad")))) NoComment'
 summer='Row 3 (Single (TimeSeason (ncn season_2_N) (FNullary (VS "Summer")))) (Single (Attribute (ncn dish_2_N) (FNullary (VS  "Roasted potatoes and a nice steak")))) NoComment'
@@ -9,7 +24,13 @@ other='Row 5 (Many (BaseFCell (TimeSeason (ncn season_2_N) FAnything) (AmountCou
 
 table="l -bind -treebank Table (ConsDTRow ($winter) (ConsDTRow ($spring) (ConsDTRow ($summer) (BaseDTRow ($autumn) ($other)))))"
 
-echo "gr -number=10 Table ? | l -treebank -bind" | gf --run DMN.pgf | sed -E 's/ \\ /\\/g ; s/: /\\---\\/g ; s/DMN/\\DMN/g' | tr '\' '\n'
+gflin() {
+    gf --run DMN.pgf \
+        | sed -E 's/ \\ /\\/g ; s/: /\\---\\/g ; s/DMN/\\DMN/g' \
+        | tr '\' '\n'
+}
 
-echo $table | gf --run DMN.pgf | sed -E 's/ \\ /\\/g ; s/: /\\---\\/g ; s/DMN/\\DMN/g' | tr '\' '\n'
+# Uncomment if you want to see 10 randomgenerated tables
+# echo "gr -number=10 Table ? | l -treebank -bind" | gflin
 
+echo $table | gflin
