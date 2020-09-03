4f4ddc1820b2f1b5b3384c7965c7c2ee993478dd
Jacob Tan En <2jt.smu@gmail.com>
2020-09-04 05:33:08 +0800

    Python generator masquerading as JS.hs

--- languages/haskell/src/DMN/Translate/JS.hs
+++ languages/haskell/src/DMN/Translate/JS.hs
@@ -27,15 +27,15 @@ toJS jsopts dt =
                                   then mkProps jsopts (tableName dt) (getInputHeaders $ header dt)
                                        ++ ([": " ++ returnName (tableName dt) | typescript jsopts])
                                   else mkArguments jsopts (header dt)
-                                , [ "{"]
+                                , [ "#{"]
                                 ] ]
              ++ zipWith (\if_ dtrow -> mkIf jsopts (hitpolicy dt) if_ (header dt) dtrow) elsif (datarows dt)
-             ++ [ "}" ]
+             ++ [ "#}" ]
   where
     elsif = "if" : repeat ( case hitpolicy dt of
-                               HP_Unique    -> "else if"
-                               HP_First     -> "else if"
-                               HP_Priority  -> "else if"
+                               HP_Unique    -> "elif"
+                               HP_First     -> "elif"
+                               HP_Priority  -> "elif"
                                HP_Collect _ -> "if"
                                _            -> "if")
   
@@ -52,7 +52,7 @@ mkTypeSpec jsopts specname chs =
   ["}"]
 
 mkFunction :: String -> [String]
-mkFunction tablename = [ "export", "function", underscore tablename ]
+mkFunction tablename = [ "def", underscore tablename ]
 
 mkProps :: JSOpts -> String -> [ColHeader] -> [String]
 mkProps jsopts tablename chs = ["(", "props" ++ if typescript jsopts then " : " ++ propsName tablename else "", ")"]
@@ -64,7 +64,7 @@ returnName :: String -> String
 returnName tablename = "Return_" ++ underscore tablename
 
 mkArguments :: JSOpts -> [ColHeader] -> [String]
-mkArguments jsopts chs = ["(", intercalate ", " (mkArgument jsopts <$> input_headers chs), ")"]
+mkArguments jsopts chs = ["(", intercalate ", " (mkArgument jsopts <$> input_headers chs), "):"]
 
 mkArgument :: JSOpts -> ColHeader -> String
 mkArgument jsopts ch = var_name ch ++ maybe "" (if typescript jsopts then (" : " ++) . type2js else const "") (vartype ch)
@@ -81,12 +81,12 @@ mkIf jsopts hp ifword chs dtrow =
   in
     "  " ++ ifword ++ " (" ++
     (if not (null conditions)
-     then intercalate " && " conditions
+     then intercalate " and " conditions
      else "\"default\"") -- TODO: tweak ifword to allow just an "else" here, rather than exploiting the truthiness of JS
-    ++ ") { // " ++
+    ++ "): # " ++
     maybe "cont'd" show (row_number dtrow) ++ "\n" ++
     (let feelout = feel2jsOut hp chs dtrow
-         standard = maybe "" (\infra -> "    " ++ infra ++ "\n") (fst feelout) ++ "    return {" ++ intercalate ", " (snd feelout) ++ "};"
+         standard = maybe "" (\infra -> "    " ++ infra ++ "\n") (fst feelout) ++ "    return {" ++ intercalate ", " (snd feelout) ++ "}"
      in
      case hp of
        HP_Unique    -> standard
@@ -100,7 +100,7 @@ mkIf jsopts hp ifword chs dtrow =
     )
     ++ "\n"
     ++ annotationsAsComments chs dtrow
-    ++ "  }"
+    ++ "  #}"
 
 -- if the row has multiple annotation columns, show the varname of the column header.
 -- if there is only one visible annotation column, hide the varname of the column header.
@@ -109,10 +109,10 @@ annotationsAsComments chs dtrow =
   let prefixedComments = catMaybes $ zipWith (\cheader commentcol -> ((varname cheader ++ ": ") ++) <$> commentcol) (comment_headers chs) (row_comments dtrow)
       unprefixed = catMaybes $ row_comments dtrow
   in
-  unlines $ ("    // "++) <$> (if length unprefixed > 1 then prefixedComments else unprefixed)
+  unlines $ ("    # "++) <$> (if length unprefixed > 1 then prefixedComments else unprefixed)
 
 fexp2js :: JSOpts -> ColHeader -> [FEELexp] -> String
-fexp2js jsopts ch fexps = wrapParen " || " (feel2jsIn ( showVarname jsopts ch) <$> fexps)
+fexp2js jsopts ch fexps = wrapParen " or " (feel2jsIn ( showVarname jsopts ch) <$> fexps)
 
 showVarname :: JSOpts -> ColHeader -> String
 showVarname jsopts ch
@@ -138,14 +138,14 @@ comment_headers = filter ((DTCH_Comment==).label)
 
 feel2jsIn :: String -> FEELexp -> String
 feel2jsIn lhs  FAnything = wrapParen "||" ["true",lhs]
-feel2jsIn lhs (FSection Feq (VB rhs))  = lhs ++ "===" ++ (toLower <$> show rhs)
-feel2jsIn lhs (FSection Feq (VN rhs))  = lhs ++ "===" ++ show rhs
-feel2jsIn lhs (FSection Feq (VS rhs))  = lhs ++ "===" ++ show rhs
+feel2jsIn lhs (FSection Feq (VB rhs))  = lhs ++ "==" ++ (toLower <$> show rhs)
+feel2jsIn lhs (FSection Feq (VN rhs))  = lhs ++ "==" ++ show rhs
+feel2jsIn lhs (FSection Feq (VS rhs))  = lhs ++ "==" ++ show rhs
 feel2jsIn lhs (FSection Flt  (VN rhs)) = lhs ++ " < "  ++ show rhs
 feel2jsIn lhs (FSection Flte (VN rhs)) = lhs ++ " <="  ++ show rhs
 feel2jsIn lhs (FSection Fgt  (VN rhs)) = lhs ++ " > "  ++ show rhs
 feel2jsIn lhs (FSection Fgte (VN rhs)) = lhs ++ " >="  ++ show rhs
-feel2jsIn lhs (FInRange lower upper)   = wrapParen " && " [show lower ++ "<=" ++ lhs, lhs ++ "<=" ++ show upper]
+feel2jsIn lhs (FInRange lower upper)   = wrapParen " and " [show lower ++ "<=" ++ lhs, lhs ++ "<=" ++ show upper]
 feel2jsIn lhs (FNullary rhs)           = feel2jsIn lhs (FSection Feq rhs)
 
 -- TODO:
@@ -180,14 +180,14 @@ showFeel :: FEELexp -> String
 showFeel (FNullary (VS str))  = show str
 showFeel (FNullary (VN num))  = show num
 showFeel (FNullary (VB bool)) = toLower <$> show bool
-showFeel (FSection Feq  (VB rhs)) = "(x)=>x === "++ (toLower <$> show rhs)
-showFeel (FSection Feq  (VS rhs)) = "(x)=>x === "++ show rhs
-showFeel (FSection Feq  (VN rhs)) = "(x)=>x === "++ show rhs
+showFeel (FSection Feq  (VB rhs)) = "(x)=>x == "++ (toLower <$> show rhs)
+showFeel (FSection Feq  (VS rhs)) = "(x)=>x == "++ show rhs
+showFeel (FSection Feq  (VN rhs)) = "(x)=>x == "++ show rhs
 showFeel (FSection Flt  (VN rhs)) = "(x)=>x < "++show rhs
 showFeel (FSection Flte (VN rhs)) = "(x)=>x <="++show rhs
 showFeel (FSection Fgt  (VN rhs)) = "(x)=>x > "++show rhs
 showFeel (FSection Fgte (VN rhs)) = "(x)=>x >="++show rhs
-showFeel (FInRange lower upper)   = "(x)=>" ++ show lower ++ "<= x && x <= " ++ show upper
+showFeel (FInRange lower upper)   = "(x)=>" ++ show lower ++ "<= x and x <= " ++ show upper
 showFeel (FFunction (FNF1 var))     = var
 showFeel (FFunction (FNF0 (VS str))) = "\"" ++ str ++ "\""
 showFeel (FFunction (FNF0 (VB bool))) = toLower <$> show bool
